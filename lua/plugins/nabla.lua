8bd6291a1622927bf4afec5ad0596827cd232ee5
Argha Chakrabarty <ac380012@gmail.com>
2023-03-30 11:46:45 +0530

    delete prev

--- lua/plugins/nabla.lua
+++
@@ -1,3 +0,0 @@
-local utils = require("utils")
-
-utils.map('n', '<C-p>', '<cmd>lua require("nabla").popup()<CR>', {noremap = true, silent = true})
--- lua/plugins/vimtex.lua
+++
@@ -1,3 +0,0 @@
-vim.g.vimtex_quickfix_enabled = 0
-vim.g.vimtex_quickfix_mode = 0
-vim.g.vimtex_quickfix_open_on_warning = 0
