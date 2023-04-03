0221f7e102db467becfef142d56e4561965b0e43
Argha Chakrabarty <ac380012@gmail.com>
2023-04-03 19:41:02 +0530

    Added friendly snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,13 +2,13 @@ return { -- Autocompletion
   'hrsh7th/nvim-cmp',
   opts = {},
   dependencies = {
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-buffer",
+    'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-nvim-lsp',
     'jcha0713/cmp-tw2css',
     'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-path',
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
+    'rafamadriz/friendly-snippets'
   },
 }
