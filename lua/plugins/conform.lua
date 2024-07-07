db492ba317f3ec786ac8237d4977f489e395a9b1
Argha Chakrabarty <ac380012@gmail.com>
2024-07-07 18:22:59 +0530

    added format enable disable commands, and optionally using biome as linter / formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,5 +1,24 @@
 local slow_format_filetypes = {}
 
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
+
 return { -- Autoformat
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
@@ -17,6 +36,9 @@ return { -- Autoformat
 	opts = {
 		notify_on_error = false,
 		format_on_save = function(bufnr)
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
 			if slow_format_filetypes[vim.bo[bufnr].filetype] then
 				return
 			end
@@ -29,6 +51,9 @@ return { -- Autoformat
 			return { timeout_ms = 200, lsp_fallback = true }, on_format
 		end,
 		format_after_save = function(bufnr)
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
 			if not slow_format_filetypes[vim.bo[bufnr].filetype] then
 				return
 			end
@@ -45,10 +70,36 @@ return { -- Autoformat
 				end
 			end,
 			rust = { "rustfmt" },
-			javascript = { { "prettierd", "prettier" }, "eslint_d" },
-			javascriptreact = { { "prettierd", "prettier" }, "eslint_d" },
-			typescript = { { "prettierd", "prettier" }, "eslint_d" },
-			typescriptreact = { { "prettierd", "prettier" }, "eslint_d" },
+			javascript = function()
+				if require("conform").get_formatter_info("biome") then
+					return { "biome" }
+				else
+					return { "prettierd", "prettier", "eslint_d" }
+				end
+			end,
+			typescript = function()
+				if require("conform").get_formatter_info("biome") then
+					return { "biome" }
+				else
+					return { "prettierd", "prettier", "eslint_d" }
+				end
+			end,
+			javascriptreact = function()
+				if require("conform").get_formatter_info("biome") then
+					return { "biome" }
+				else
+					return { "prettierd", "prettier", "eslint_d" }
+				end
+			end,
+			typescriptreact = function()
+				if require("conform").get_formatter_info("biome") then
+					return { "biome" }
+				else
+					return { "prettierd", "prettier", "eslint_d" }
+				end
+			end,
+			-- javascriptreact = { { "prettierd", "prettier" }, "eslint_d" },
+			-- typescriptreact = { { "prettierd", "prettier" }, "eslint_d" },
 			--
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
